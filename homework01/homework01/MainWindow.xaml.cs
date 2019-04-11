a7a6e1ba7da031610f291a09c69f04ee3fc8a805
Jing Wang <jinglywang@gmail.com>
2019-04-11 00:14:09 -0700

    create hello world wpf application

---
+++ homework01/homework01/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace homework01
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void uxUserName_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			onUserNamePasswordTextChanged();
+		}
+
+		private void uxPassword_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			onUserNamePasswordTextChanged();
+			
+		}
+
+		private void onUserNamePasswordTextChanged()
+		{
+			if (uxUserName.Text != "" && uxPassword.Text != "")
+			{
+				btnSubmit.IsEnabled = true;
+			}
+			else
+			{
+				btnSubmit.IsEnabled = false;
+		    }
+		}
+	}
+}
